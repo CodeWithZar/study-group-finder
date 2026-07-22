434d5635acfe690f2aaf05db86555e4118add466
Zar Khan Hilal <zarkhangamer1@gmail.com>
2026-07-22 12:24:11 +0500

    feat: add mock profile and study group data

---
+++ src/data/mockProfile.js
@@ -0,0 +1,41 @@
+export const defaultProfile = {
+  name: "",
+  email: "",
+  degree: "BSc Computer Science",
+  year: "Final Year",
+  modules: ["CS301", "CS302", "DATA201", "WEB301"],
+  availability: "Weekday Evenings",
+  studyMode: "Hybrid",
+  campus: "Main Campus",
+  goal: "Prepare for final exams and group coursework",
+};
+
+export const moduleOptions = [
+  "CS101",
+  "CS201",
+  "CS301",
+  "CS302",
+  "MATH101",
+  "DATA201",
+  "WEB301",
+  "AI401",
+  "SEC301",
+  "NET201",
+];
+
+export const availabilityOptions = [
+  "Weekday Evenings",
+  "Weekday Afternoons",
+  "Weekend Mornings",
+  "Weekend Afternoons",
+  "Flexible",
+];
+
+export const studyModeOptions = ["In-Person", "Online", "Hybrid"];
+
+export const campusOptions = [
+  "Main Campus",
+  "City Campus",
+  "Online",
+  "No Preference",
+];
